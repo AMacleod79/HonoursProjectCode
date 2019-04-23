713b72fa4eda841d02806d3b7e3ebd1f35dbe54f
AMacleod79 <couthier.annabelle@gmail.com>
2019-04-23 10:51:42 +0100

    Final setup for graphs showing variations of performance metrics for different under sampling conditions

--- VariationsGraph.R
+++ VariationsGraph.R
@@ -12,6 +12,8 @@ RFRes60Df <-read.csv("RF_results3.csv")
 #RF undersample 75%
 RFRes75Df <-read.csv("RF_results4.csv")
 
+#all the values in one dataframe for convenience
+#variations from the baseline calculated
 VariationsUnderDf <- data.frame(Dataset=RfResDf$Dataset, 
                          AccuRFBase=RfResDf$Accuracy,
                          AccuRF40 = RFRes40Df$Accuracy,
@@ -45,51 +47,131 @@ VariationsUnderDf <- data.frame(Dataset=RfResDf$Dataset,
 
 write.csv(VariationsUnderDf, file = "variations2.csv")
 
-
+#this table to show the variations from the base line for accuracy only
 accuVariationsDf <- data.frame(Dataset = RfResDf$Dataset,
                                Variation75 = VariationsUnderDf$AccuVariation75,
                                Variation60 = VariationsUnderDf$AccuVariation60,
                                Variation40 = VariationsUnderDf$AccuVariation40)
+getwd()
+list.files()
+setwd("/Users/annabelle/Documents/HonoursYear/HonoursProject/HonoursProjectCode/Performance")
+write.csv(accuVariationsDf, file = "accuVariations.csv")
 
+#this table to show the variations from the base line for sensitivity only
 sensiVariationsDf <- data.frame(Dataset = RfResDf$Dataset,
                                 Variation75 = VariationsUnderDf$SensiVariation75,
                                 Variation60 = VariationsUnderDf$SensiVariation60,
                                 Variation40 = VariationsUnderDf$SensiVariation40)
+write.csv(sensiVariationsDf, file="sensiVariations.csv")
+
+#this table to show the variations from the base line for precision only
 preciVariationsDf <- data.frame(Dataset = RfResDf$Dataset,
                                 Variation75 = VariationsUnderDf$PreciVariation75,
                                 Variation60 = VariationsUnderDf$PreciVariation60,
                                 Variation40 = VariationsUnderDf$PreciVariation40)
+write.csv(preciVariationsDf, file="preciVariatins.csv")
+
+#this table to show the variations from the base line for the F1 score only
 F1VariationsDf <- data.frame(Dataset = RfResDf$Dataset,
                              Variation75 = VariationsUnderDf$F1Variation75,
                              Variation60 = VariationsUnderDf$F1Variation60,
                              Variation40 = VariationsUnderDf$F1Variation40)
+write.csv(F1VariationsDf, file = "F1Variations.csv")
 
-accuVariationsPlot <- data.frame(Dataset = c("Autism", "Autism", "Autism",
-                                            "Breast Cancer", "Breast Cancer", "Breast Cancer",
-                                            "Cervical Cancer", "Cervical Cancer", "Cervical Cancer",
-                                            "Diabetes", "Diabetes", "Diabetes",
-                                            "Fertility", "Fertility", "Fertility",
-                                            "Heart Attack", "Heart Attack", "Heart Attack",
-                                            "Low Back Pain", "Low Back Pain", "Low Back Pain",
-                                            "Liver", "Liver", "Liver",
-                                            "modHA","modHA", "modHA",
-                                            "modLBP","modLBP", "modLBP"),
-                                 Condition = c("75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %",
-                                               "75 %", "60 %", "40 %"),
+
+#change directory to save graphs
+setwd("/Users/annabelle/Documents/HonoursYear/HonoursProject/HonoursProjectCode/Figures")
+
+#this table to gather all the accuracy data for the ggplot
+accuVariationsPlot <- data.frame(Dataset = c("Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                             "Low Back Pain","Liver","modHA","modLBP",
+                                             "Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                             "Low Back Pain","Liver","modHA","modLBP",
+                                             "Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                             "Low Back Pain","Liver","modHA","modLBP"),
+                                 Condition = c("75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", 
+                                               "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %",
+                                               "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %"),
                                  Variation = c(VariationsUnderDf$AccuVariation75,
                                                VariationsUnderDf$AccuVariation60,
                                                VariationsUnderDf$AccuVariation40))
+
+#wrap Dataset names to look pretty on the graph
+library(stringr)
+accuVariationsPlot$wrappedData = str_wrap(accuVariationsPlot$Dataset, width=10)
+library("RColorBrewer")
+display.brewer.all()
 library(ggplot2)
-ggplot(data = accuVariationsPlot, aes(x=Condition, y = Variation , fill = Dataset))+ geom_bar(stat="identity",position = position_dodge())
-ggsave("Figures/Chapter5/AccuVariationUnder.png", width = 16, height =25, dpi = 200)
-#need to change the colours to make it  more legible, also change the aspect ratio?
-#repeat for other metrics
+
+#this graphs displays the variation in accuracy grouped by condition on the x axis
+ggplot(data = accuVariationsPlot, aes(x=Condition, y = Variation , fill = Dataset))+ geom_bar(stat="identity",position = position_dodge()) + scale_fill_brewer(palette="Set3")
+ggsave("Chapter5/AccuVariationUnder.png", width = 5, height =10, dpi = 300)
+
+install.packages("wesanderson")
+library(wesanderson)
+#this graphs displays the variations in accuracy grouped by dataset on the x axis
+ggplot(data = accuVariationsPlot, aes(x=wrappedData, y = Variation, fill = Condition)) + geom_bar(stat="identity", position = position_dodge()) + scale_fill_manual(values=wes_palette(n=3, name="GrandBudapest1")) + xlab("Datasets") + ylab("Variation")
+ggsave("Chapter5/AccuVariationUnderBySets.png", width = 8, height =10, dpi = 300)
+
+
+#this table to gather all the sensitivity data for the ggplot
+sensiVariationsPlot <- data.frame(Dataset = c("Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                              "Low Back Pain","Liver","modHA","modLBP",
+                                              "Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                              "Low Back Pain","Liver","modHA","modLBP",
+                                              "Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                              "Low Back Pain","Liver","modHA","modLBP"),
+                                  Condition = c("75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", 
+                                                "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %",
+                                                "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %"),
+                                  Variation = c(VariationsUnderDf$SensiVariation75,
+                                                VariationsUnderDf$SensiVariation60,
+                                                VariationsUnderDf$SensiVariation40))
+
+#wrap Datasets names to look pretty on the graph
+sensiVariationsPlot$wrappedData = str_wrap(sensiVariationsPlot$Dataset, width = 10)
+#this graphs displays the variations in sensitivity grouped by dataset on the x axis
+ggplot(data = sensiVariationsPlot, aes(x=wrappedData, y= Variation, fill = Condition)) +geom_bar(stat="identity", position = position_dodge()) + scale_fill_manual(values = wes_palette(n=3, name = "GrandBudapest1")) + xlab("Datasets") + ylab("Variation")
+ggsave("Chapter5/SensiVariationUnderBySets.png", width = 8, height =10, dpi = 300)
+
+
+#this table to gather all the precision data for the ggplot
+preciVariationsPlots <- data.frame(Dataset = c("Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                               "Low Back Pain","Liver","modHA","modLBP",
+                                               "Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                               "Low Back Pain","Liver","modHA","modLBP",
+                                               "Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                               "Low Back Pain","Liver","modHA","modLBP"),
+                                   Condition = c("75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", 
+                                                 "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %",
+                                                 "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %"),
+                                   Variation = c(VariationsUnderDf$PreciVariation75,
+                                                 VariationsUnderDf$PreciVariation60,
+                                                 VariationsUnderDf$PreciVariation40))
+#wrap datasets names to look pretty
+preciVariationsPlots$wrappedData = str_wrap(preciVariationsPlots$Dataset, width = 10)
+#this graphs displays the variations in precisions grouped by dataset on the x axis
+ggplot(data = preciVariationsPlots, aes(x=wrappedData, y = Variation, fill= Condition)) + geom_bar(stat="identity", position = position_dodge()) + scale_fill_manual(values = wes_palette(n=3, name = "GrandBudapest1")) + xlab("Datasets") + ylab("Variation")
+ggsave("Chapter5/PreciVariationUnderBySets.png", width = 8, height=10, dpi=300)
+
+
+#this table to gather all the F1 score data for the ggplot
+F1VariationsPlots <- data.frame(Dataset = c("Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                            "Low Back Pain","Liver","modHA","modLBP",
+                                            "Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                            "Low Back Pain","Liver","modHA","modLBP",
+                                            "Autism","Breast Cancer","Cervical Cancer", "Diabetes", "Fertility","Heart Attack",
+                                            "Low Back Pain","Liver","modHA","modLBP"),
+                                Condition = c("75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", "75 %", 
+                                              "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %", "60 %",
+                                              "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %", "40 %"),
+                                Variation = c(VariationsUnderDf$F1Variation75,
+                                              VariationsUnderDf$F1Variation60,
+                                              VariationsUnderDf$F1Variation40))
+
+#wrap datasets names to look pretty
+F1VariationsPlots$wrappedData = str_wrap(F1VariationsPlots$Dataset, width = 10)
+#this graphs displays the variations in F1 score grouped by dataset on the x axis
+ggplot(data = F1VariationsPlots, aes(x = wrappedData, y = Variation, fill = Condition)) + geom_bar(stat="identity", position = position_dodge()) + scale_fill_manual(values = wes_palette(n=3, name="GrandBudapest1")) + xlab("Datasets") + ylab("Variation")
+ggsave("Chapter5/F1VariationUnderBySets.png", width = 8, height = 10, dpi = 300)
 
